2541b8747e5276902910315fc11f48dce1699b82
Skilled <skilled5041@gmail.com>
2023-03-04 14:50:37 -0500

    Added logos and changed colours

--- src/routes/+layout.svelte
+++ src/routes/+layout.svelte
@@ -5,10 +5,10 @@
     export let data;
 </script>
 
-<div class="navbar mb-2 shadow-lg">
+<div class="navbar mb-2 bg-base-200 300 shadow-lg">
     <div class="px-2 mx-2 mb-4 navbar-start">
-        <a href="/" class="ml-4 text-lg font-bold">Contest Ace
-            <a class="text-4xl">💯</a>
+        <a href="/" class="ml-4 text-lg font-bold">
+            <img src="src/imgs/logo.png" class="w-16 mt-4 px-4" alt="Contest Ace">
         </a>
     </div>
     <div class="hidden px-2 mx-2 navbar-center lg:flex">
--- src/routes/+page.svelte
+++ src/routes/+page.svelte
@@ -5,5 +5,5 @@
 {#if data.session.session}
     <h1 class="text-3xl font-bold">Welcome to Contest Ace, {data.session.session.user.user_metadata.username}!</h1>
 {:else}
-    <h1 class="text-3xl font-bold">Welcome to Contest Ace, Guest</h1>
+    <h1 class="text-3xl font-bold">Welcome to Contest Ace!</h1>
 {/if}
