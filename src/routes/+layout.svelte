cbf7a5d67fc939fc0bdd614ab5667a104a982e3a
Skilled <skilled5041@gmail.com>
2023-03-03 20:51:11 -0500

    Added navbar and changed error page

--- src/routes/+layout.svelte
+++ src/routes/+layout.svelte
@@ -2,4 +2,35 @@
     import "../app.css";
 </script>
 
-<slot />
+<div class="navbar mb-2 shadow-lg">
+    <div class="px-2 mx-2 navbar-start">
+        <a href="/" class="text-lg font-bold">
+            name
+        </a>
+    </div>
+    <div class="hidden px-2 mx-2 navbar-center lg:flex">
+        <div class="flex items-stretch">
+            <a href="/" class="btn btn-ghost btn-sm rounded-btn">
+                Home
+            </a>
+            <a href="problems" class="btn btn-ghost btn-sm rounded-btn">
+                Problems
+            </a>
+            <a href="about" class="btn btn-ghost btn-sm rounded-btn">
+                About
+            </a>
+        </div>
+    </div>
+    <div class="navbar-end px-8">
+        <button class="btn btn-sm btn-primary mx-4">
+            <a href="login">Login</a>
+        </button>
+        <button class="btn btn-sm btn-primary mx-4">
+            <a href="register">Register</a>
+        </button>
+    </div>
+</div>
+
+<div class="mx-auto py-16 px-4 text-center bg">
+    <slot/>
+</div>
--- tailwind.config.cjs
+++ tailwind.config.cjs
@@ -2,6 +2,9 @@
 module.exports = {
   content: ['./src/**/*.{html,js,svelte,ts}'],
   theme: {
+    daisyui: {
+        themes: ['emerald']
+    },
     extend: {},
   },
   plugins: [require("daisyui")],
