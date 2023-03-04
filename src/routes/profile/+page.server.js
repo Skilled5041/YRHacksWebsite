3b63ff7caa858c62771b2396be1dc1bc0c1ff5c9
Skilled <skilled5041@gmail.com>
2023-03-04 14:10:34 -0500

    Basic profile information

--- src/routes/profile/+page.server.js
+++ src/routes/profile/+page.server.js
@@ -1,9 +1,28 @@
 import { redirect } from "@sveltejs/kit";
+import { supabase } from "$lib/supabase";
 
 export const load = async ({locals}) => {
     const session = await locals.getSession();
     if (!session) {
         throw redirect(303, "/login");
     }
-    return {};
-}
+
+    let {data: problemsSolved, error: err} = await supabase
+        .from("profiles")
+        .select("problems_solved")
+        .eq("id", session.user.id);
+
+    let {data: points, error: err2} = await supabase
+        .from("profiles")
+        .select("points")
+        .eq("id", session.user.id);
+
+    if (err) {
+        return {problemsSolved: 'Error loading data'};
+    }
+
+    return {
+        problemsSolved: problemsSolved[0].problems_solved,
+        points: points[0].points
+    };
+};
--- src/routes/profile/+page.svelte
+++ src/routes/profile/+page.svelte
@@ -1,3 +1,12 @@
 <script>
+
     export let data;
+    const user = data.session.session.user;
+
 </script>
+
+<h1 class="font-bold text-4xl mt-32">{user.user_metadata.username}</h1>
+<hr class="h-px mt-2">
+<h1 class="mt-8">Problems solved: {data.problemsSolved}</h1>
+<h1 class="mt-2">Points: {data.points}</h1>
+<h1 class="mt-2">Rank: </h1>
